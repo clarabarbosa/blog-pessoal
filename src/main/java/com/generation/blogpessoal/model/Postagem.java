2eb604b9c283f266119659d5894c52d88c3d07ca
Maria Clara Barbosa <mah201218@gmail.com>
2025-01-30 10:40:52 -0300

    Reiniciando

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,85 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity                        // indicar que a classe é uma tabela
+@Table(name = "tb_postagens") // indicar o nome da tabela no bd
+public class Postagem {
+	
+	@Id // Ele é a chave primária (PRIMARY KEY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Auto incremento
+	private Long id;
+	
+	@NotBlank(message = "O atributo título é obrigatório!") // "Titulo da postagem"
+	@Size(min = 5, max = 100, message = "O atributo titulo deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotBlank(message = "O atributo texto é obrigatório") // "Titulo da postagem"
+	@Size(min = 5, max = 100, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data; // 10/10/25 18:25:58
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+	
+	public Long getId() {
+		return this.id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return this.titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return this.texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public LocalDateTime getData() {
+		return this.data;
+	}
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+
+	
+	
+}
